f442bffa30a6e82873234fa70174b90f37af18c9
asconem <asconem@gmail.com>
2020-03-03 18:09:37 -0500

    Adding person class.

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,37 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void testConstructorID() {
+        Person person = new Person(2345, "John");
+        long expected = 2345;
+        long actual = person.getId();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorName() {
+        Person person = new Person(2345, "John");
+        String expected = "John";
+        String actual = person.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetName() {
+        Person person = new Person(4567, "Bob");
+        person.setName("Jack");
+
+        String expected = "Jack";
+        String actual = person.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
+
