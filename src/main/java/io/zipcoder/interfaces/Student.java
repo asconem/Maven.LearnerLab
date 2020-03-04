beecfa67fbd60333ec14fb5ea3bbe5615f237c57
asconem <asconem@gmail.com>
2020-03-03 19:26:21 -0500

    Adding person class.

---
+++ src/main/java/io/zipcoder/interfaces/Student.java
@@ -0,0 +1,21 @@
+package io.zipcoder.interfaces;
+
+public class Student extends Person implements Learner {
+    private double totalStudyTime;
+
+    public Student(long id, String name) {
+        super(id, name);
+        totalStudyTime = 0;
+    }
+
+    @Override
+    public void learn(double numberOfHours) {
+    totalStudyTime += numberOfHours;
+    }
+
+    @Override
+    public Double getTotalStudyTime() {
+        return totalStudyTime;
+    }
+
+}
