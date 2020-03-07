d8fd66f9efb65039d597f0c047691cb865c6cd15
asconem <asconem@gmail.com>
2020-03-06 22:12:03 -0500

    Saving ZCW class

--- src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
+++ src/main/java/io/zipcoder/interfaces/ZipCodeWilmington.java
@@ -1,4 +1,26 @@
 package io.zipcoder.interfaces;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ZipCodeWilmington {
+    private Students students = Students.getInstance();
+    private Instructors instructors = Instructors.getInstance();
+
+    public void hostLecture(Teacher teacher, double numberOfHours) {
+        teacher.lecture(students.getArray(), numberOfHours);
+    }
+
+    public void hostLecture(long id, double numberOfHours){
+        Teacher teacher = instructors.findById(id);
+        hostLecture(teacher, numberOfHours);
+    }
+
+    public Map<Student, Double> getStudyMap() {
+        Map<Student, Double>  map = new HashMap<>();
+        for(Student s : students.getArray()){
+            map.put(s, s.getTotalStudyTime());
+        }
+        return map;
+    }
 }
